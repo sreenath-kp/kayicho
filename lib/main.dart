e354ffadf59e8621c89848e9fc2db51ec60518dc
Sreenath <108588566+sreenath-kp@users.noreply.github.com>
2023-07-21 17:10:15 +0530

    google auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:kayicho/provider/google_signin.dart';
 import 'package:kayicho/screens/bill_screen.dart';
 import 'package:kayicho/screens/home.dart';
 import 'package:kayicho/screens/onboarding.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:kayicho/screens/spashscreen.dart';
+import 'package:provider/provider.dart';
+// import 'package:kayicho/services/auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,14 +20,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        // '/': (context) => const HomeScreen(),
-        '/': (context) => const OnBoardingScreen(),
-        '/home': (context) => const HomeScreen(),
-        '/bill': (context) => const BillScreen(),
-      },
+    // final auth = Auth();
+    return ChangeNotifierProvider(
+      create: (context) => GoogleSignInProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/home': (context) => HomeScreen(),
+          '/bill': (context) => const BillScreen(),
+        },
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (contex, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const SplashScreen();
+            }
+            if (snapshot.hasData) {
+              return HomeScreen();
+            }
+            return OnBoardingScreen();
+          },
+        ),
+      ),
     );
   }
 }
--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,49 +1,106 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:kayicho/provider/google_signin.dart';
 import 'package:kayicho/services/auth.dart';
+import 'package:provider/provider.dart';
 
-class OnBoardingScreen extends StatelessWidget {
+class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
 
   @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  final _textController = TextEditingController();
+
+  bool isLoading = false;
+
+  @override
   Widget build(BuildContext context) {
-    final Auth _auth = Auth();
-    return Container(
-      color: Colors.black,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Icon(Icons.food_bank, color: Colors.white),
-          ElevatedButton(
-            onPressed: () async {
-              dynamic result = await _auth.signInAnon();
-              if (result == null) {
-                print('Error in Signing in');
-              } else {
-                Navigator.pushReplacementNamed(context, '/home');
-                print('Signed in');
-                print(result.uid);
-              }
-            },
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.white),
-              shape: MaterialStateProperty.all(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25),
-                ),
+    final Auth auth = Auth();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: isLoading
+            ? const CircularProgressIndicator(
+                color: Colors.white,
+              )
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.food_bank,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  // Padding(
+                  //   padding:
+                  //       const EdgeInsets.only(left: 50, right: 50, bottom: 20, top: 20),
+                  //   child: TextField(
+                  //     maxLines: 1,
+                  //     cursorColor: Colors.black,
+                  //     style: const TextStyle(color: Colors.black),
+                  //     controller: _textController,
+                  //     decoration: InputDecoration(
+                  //       constraints: const BoxConstraints(maxHeight: 45),
+                  //       focusedBorder: OutlineInputBorder(
+                  //         borderSide: BorderSide.none,
+                  //         borderRadius: BorderRadius.circular(10),
+                  //       ),
+                  //       border: OutlineInputBorder(
+                  //         borderSide: BorderSide.none,
+                  //         borderRadius: BorderRadius.circular(10),
+                  //       ),
+                  //       fillColor: Colors.white,
+                  //       filled: true,
+                  //       labelText: 'What is your name ?',
+                  //       labelStyle: GoogleFonts.inter(
+                  //         color: Colors.black,
+                  //         fontSize: 16,
+                  //         fontWeight: FontWeight.w300,
+                  //       ),
+                  //       floatingLabelBehavior: FloatingLabelBehavior.never,
+                  //       prefixIcon: const Icon(Icons.person_2_outlined),
+                  //       prefixIconColor: Colors.grey,
+                  //     ),
+                  //   ),
+                  // ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      setState(() {
+                        isLoading = true;
+                      });
+
+                      // if (_textController.text.trim().isNotEmpty) {
+                      final provider = Provider.of<GoogleSignInProvider>(
+                          context,
+                          listen: false);
+                      await provider.googleLogin();
+                      setState(() {
+                        isLoading = false;
+                      });
+                      // }
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      'Get Started',
+                      style: GoogleFonts.inter(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  )
+                ],
               ),
-            ),
-            child: Text(
-              'Get Started',
-              style: GoogleFonts.inter(
-                color: Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          )
-        ],
       ),
     );
   }
